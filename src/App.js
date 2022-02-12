d076e2e1b31f2318e239b1f165b1a89d88e75c43
Ashley N Dennis <a.n.d@Ashleys-MacBook-Air.local>
2022-02-12 11:15:11 -0500

    firsst push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [login, setLoggedIn] = useState(false);
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {login && <h1>Welcome Back!</h1>}
+        {!login &&<h1>Please Login </h1>}
+        <button
+        onClick={() => setLoggedIn(true)}
+        > Login </button>
+        &nbsp;
+        <button
+        onClick={() => setLoggedIn(false)} 
+        >Logout</button>
+        &nbsp;
+        
       </header>
     </div>
   );
-}
-
+      }
 export default App;
